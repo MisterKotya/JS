477338bc7b321f44871dc04f39f0271e1380b0cf
Dima Chuprov <chuprov555@mail.ru>
2022-04-21 17:46:57 +0300

    1

---
+++ Unit_2/Material_2/script.js
@@ -0,0 +1,59 @@
+let a = 6;
+let b = 'Hello';
+
+console.log(a);
+
+t = parseInt(t)
+parseInt("string", radix); //ф-ция преобразования принимает два аргумента(строку и основание системы) из строки в число и игнорит e**n(экспонент в стемени n)
+// string - строка, записанная в виде числа "12px" вывод в консоли 12, radix - основание системы счисления
+//parseInt("12px") => 12    parseInt("Hello12.5") =>  Nan   parseInt(+12.5Hello) => "+12.5"(преобразование в строку) => без radix считает по десятичной системе исчисления => +12
+// parseInt("0123", 8) => 83 - высчитано по восьмеричной системе исчисления
+
+t = parseFloat(t)
+parseFloat("string"); // ф-ция преобразования принимает только один аргумент из строки в число или значение Nan, как это получается в данном варианте
+// parseFloat игнорит все символы, за исключением 0-9, -1 +2 12.5 e**n(экспонент в стемени n)
+// parseFloat("3.14") => 3.14   parseFloat("314e-2") => 3.14   parseFloat("e23") => Nan    parseFloat("0x12") => 0   parseFloat("0123") => 123 - считает по десятиричной системе исчисления
+
+
+let inputIn = document.querySelector('.input-in'); // захват тэга input и правило написания так inputIn - называется camelcase
+let button = document.querySelector('button'); // захват тэга button
+let div = document.querySelector('.out');
+
+// в JS, н-р кликаю по кнопке Go(что то произошло - это событие click) и я что-то запускаю, какой-то кусочек кода, т.е. запускаю процедуру или выполняю функцию.
+//Чтобы что-то произошло в JS, нужно использовавть событие и функцию.
+
+button.onclick = function () { // button - кнопка и oncklick - событие(кликаем на кнопку) = function(выполняется функция)  
+    // кнопка будет нажата, функция будет выполнена и никак иначе и в консоль выпадет Работает!!!!!!
+    console.log('Работает!!!!!!');
+    // console.log(inputIn.value); // value - это то, что введено в input - научился читать значение из input !!!
+    let b = +inputIn.value; // + преобразование в число!!!!!!!
+    // console.log(b * 2); // при вводе в input Hello на выходе получаем не число - Nan
+    // console.log(b / 0); // число при выводе даёт Infinity именно в JS, а если строка, то Nan
+    console.log(b + 10); // изначально склеивается, потому что первый элемент строчный b10, но когда let b = +inputIn.value; то выводится число.
+    div.innerHTML = b; // вывод на страницу
+    inputIn.value = ''; // присвоение очистки input после нажатия кнопки
+}
+// Создайте две переменные e = 3 и f = 5. Выведите на страницу  в .out-3 результат сложения e + f.
+let e = 3;
+let f = 5;
+document.querySelector('.out-3').textContent = e + f; // Вывод значения переменных с совершением операций через textContent на страницу
+
+// Создайте две переменные e3 = 3 и f3 = 'Hello'. Выведите на страницу  в .out-6 результат сложения двух переменных.
+let e3 = 3;
+let f3 = 'Hello';
+document.querySelector('.out-6').textContent = e3 + f3; // не смотря на то, что строка идет вторая, мы получаем в итоге строку, потому что мы не можем ее приравнять к числу
+
+// Создайте input и кнопку. По нажатию на кнопку выполняется функция, которая выводит в консоль то, что пользователь ввел в input.
+let inputT8 = document.querySelector('.inputT8');
+function t8() {
+    console.log(inputT8.value);
+}
+document.querySelector('.b-8').onclick = t8;
+
+function t9() {
+    let valueOfT9 = inputT9.value; // создаем отдельную переменную для inputT9.value;(набор ввода разных чисел в строке)
+    outputB9.innerHTML = valueOfT9; // производим через outputB9 вывод на страницу .innerHTML всех разных набранных чисел в строке inputT9.value;, посредством раннее присвоенной переменной valueOfT9
+    inputT9.value = '';
+}
+
+document.querySelector('.b-9').onclick = t9;
---
+++ Unit_2/Test_DZ_2/unit_02.js
@@ -0,0 +1,193 @@
+// Task 1.
+// Создайте две переменные a = 7 и b = 9. Выведите в консоль результа умножения a на b.
+let a = 7;
+let b = 9;
+console.log(a * b);
+
+// Task 2.
+// Создайте две переменные c = 7 и d = 9. Выведите на страницу в .out-2 результат деления c на d.
+let c = 7;
+let d = 9;
+document.querySelector('.out-2').textContent = c / d;
+
+// Task 3.
+// Создайте две переменные e = 3 и f = 5. Выведите на страницу  в .out-3 результат сложения e + f.
+let e = 3;
+let f = 5;
+document.querySelector('.out-3').textContent = e + f;
+
+// Task 4.
+// Создайте две переменные e1 = '3' и f1 = 5. Выведите на страницу  в .out-4 результат сложения e1 + f1. Объясните разницу.
+let e1 = '3';
+let f1 = 5;
+document.querySelector('.out-4').textContent = '3' + 5;
+
+// Task 5.
+// Создайте две переменные e2 = 3 и f2 = 0. Выведите на страницу  в .out-5 результат деления e2 на f2.
+let e2 = 3;
+let f2 = 0;
+document.querySelector('.out-5').textContent = e2 / f2; // Infinity
+
+// Task 6.
+// Создайте две переменные e3 = 3 и f3 = 'Hello'. Выведите на страницу  в .out-6 результат сложения двух переменных.
+let e3 = 3;
+let f3 = 'Hello';
+document.querySelector('.out-6').textContent = e3 + f3; // не смотря на то, что строка идет вторая, мы получаем в итоге строку, потому что мы не можем ее приравнять к числу
+
+// Task 7.
+// Создайте две переменные e4 = 3 и f4 = 'Hello'. Выведите на страницу  в .out-7 результат умножения двух переменных.
+let e4 = 3;
+let f4 = 'Hello';
+document.querySelector('.out-7').textContent = e4 * f4; // 3*Nan=Nan
+
+// Task 8.
+// Создайте input и кнопку. По нажатию на кнопку выполняется функция, которая выводит в консоль то, что пользователь ввел в input.
+let inputT8 = document.querySelector('.inputT8');
+
+function t8() {
+    console.log(inputT8.value);
+}
+
+document.querySelector('.b-8').onclick = t8;
+
+// Task 9.
+// Создайте input и кнопку. По нажатию на кнопку выполняется функция, которая выводит на страницу,  что пользователь ввел в input. Добавьте очистку input после нажатия кнопки.
+let inputT9 = document.querySelector('.i-9');
+let outputB9 = document.querySelector('.out-9');
+
+function t9() {
+    let valueOfT9 = inputT9.value; // создаем отдельную переменную для inputT9.value;(набор ввода разных чисел в строке)
+    outputB9.innerHTML = valueOfT9; // производим через outputB9 вывод на страницу .innerHTML всех разных набранных чисел в строке inputT9.value;, посредством раннее присвоенной переменной valueOfT9
+    inputT9.value = '';
+}
+
+document.querySelector('.b-9').onclick = t9;
+
+// Task 10.
+// Создайте input и кнопку. По нажатию на кнопку выполняется функция, которая выводит на страницу число, которое ввел пользователь умноженное на 10.
+let inputT10 = document.querySelector('.i-10');
+let outputB10 = document.querySelector('.out-10');
+
+function t10() {
+    let valueof10 = inputT10.value;
+    outputB10.innerHTML = valueof10 * 10;
+}
+
+document.querySelector('.b-10').onclick = t10;
+
+// Task 11.
+// Создайте input и кнопку. По нажатию на кнопку выполняется функция, которая выводит на страницу в .out-11 число, которое ввел пользователь и к нему добавленное число 10.
+let inputT11 = document.querySelector('.i-11');
+let outputB11 = document.querySelector('.out-11');
+
+function t11() {
+    let valueof11 = inputT11.value;
+    outputB11.innerHTML = +valueof11 + 10;
+}
+
+document.querySelector('.b-11').onclick = t11;
+
+// Task 12.
+// Создайте два input (i-12_1 и i-12-2) и кнопку. В первый input пользователь вводит имя, во второе фамилию. При нажатии кнопки выполняется функция, которая выводит в .out-12 строку 'Hello имя фамилия', где имя - имя пользователя и фамилия - введенная фамилия.
+let inputT12_1 = document.querySelector('.i-12_1');
+let inputT12_2 = document.querySelector('.i-12_2');
+let outputB12 = document.querySelector('.out-12');
+
+function t12() {
+    let valueof12_1 = inputT12_1.value;
+    let valueof12_2 = inputT12_2.value;
+    outputB12.innerHTML = 'Hello' + ' ' + valueof12_1 + ' ' + valueof12_2;
+
+}
+
+document.querySelector('.b-12').onclick = t12;
+
+// Task 13.
+// Создайте два input и кнопку. В input пользователь вводит числа. При нажатии кнопки выполняется функция, которая выводит сумму данных двух чисел на страницу в .out-13.
+let inputT13_1 = document.querySelector('.i-13_1');
+let inputT13_2 = document.querySelector('.i-13_2');
+let outputB13 = document.querySelector('.out-13');
+
+function t13() {
+    let valueof13_1 = inputT13_1.value;
+    let valueof13_2 = inputT13_2.value;
+    outputB13.innerHTML = +valueof13_1 + +valueof13_2;
+
+}
+
+document.querySelector('.b-13').onclick = t13;
+
+// Task 14.
+// Создайте input .i-14 и пропишите ему в html value = 77. С помощью JS измените value на 'Hello'.
+
+document.querySelector('.i-14').value = 'Hello';
+
+// Task 15.
+// Создайте input i-15 и получите его в переменную y. В js выполните следующее присвоение: y.style.border = '2px solid red' . Изучите результат операции.
+
+let y = document.querySelector('.i-15');
+y.style.border = '2px solid red';
+
+// Task 16.
+// Создайте два input type=number, куда пользователь может ввести числа. Выведите на страницу сумму данных чисел по нажатию кнопки b-16
+let inputT16_1 = document.querySelector('.i-16-1');
+let inputT16_2 = document.querySelector('.i-16-2');
+let outputB16 = document.querySelector('.out-16');
+
+function t16() {
+    let valueof16_1 = inputT16_1.value;
+    let valueof16_2 = inputT16_2.value;
+    outputB16.innerHTML = +valueof16_1 + +valueof16_2;
+}
+
+document.querySelector('.b-16').onclick = t16;
+
+// Task 17.
+// Создайте input type="text" куда пользователь может ввести строку и число. Создайте функцию, которая запускается по нажатию на кнопку. Получите значение из input в переменную t. А затем сделайте операцию t = parseInt(t), и результат операции выведите в out-17. Прочитайте за эту операцию. Попробуйте по очереди вводить 44, 44aaa, 44.3, a45 . Изучите вывод.
+let inputT17 = document.querySelector('.i-17');
+let outputB17 = document.querySelector('.out-17');
+
+function t17() {
+    let t = inputT17.value;
+    t = parseInt(t);
+    outputB17.innerHTML = t;
+
+}
+
+document.querySelector('.b-17').onclick = t17;
+
+// Task 18.
+// Создайте input type="text" куда пользователь может ввести строку и число. Создайте функцию, которая запускается по нажатию на кнопку. Получите значение из input в переменную t. А затем сделайте операцию t = parseFloat(t), и результат операции выведите в консоль. Прочитайте за эту операцию. Попробуйте по очереди вводить 44, 44aaa, 44.3, a45 . Изучите вывод.
+let inputT18 = document.querySelector('.i-18');
+let outputB18 = document.querySelector('.out-18');
+
+function t18() {
+    let t = inputT18.value;
+    t = parseFloat(t)
+    console.log(t);
+}
+
+document.querySelector('.b-18').onclick = t18;
+
+// Task 19.
+// Создайте два input type=number, куда пользователь может ввести отрицательные числа. Выведите на страницу сумму данных чисел. Мы это делали! Зачем? Затем, что нужно понимать как влияет перевод в число с помощью + и parseInt на отрицательные числа!!!
+let inputT19_1 = document.querySelector('.i-19-1');
+let inputT19_2 = document.querySelector('.i-19-2');
+let outputB19 = document.querySelector('.out-19');
+
+function t19() {
+    let t = inputT19_1.value + inputT19_2.value;
+    t = parseInt(t);
+    outputB19.innerHTML = t;
+}
+
+document.querySelector('.b-19').onclick = t19;
+
+// Task 20
+// Создайте опросник, куда пользователь может ввести имя, фамилию, возраст, род занятий. И кнопку. По нажатию кнопки выведите на страницу предложение 'Уважаемый Иван, Иванов, ваш возраст 33 года, по професси вы ...' куда поставьте соответствующие данные из inputов.
+
+function t20() {
+
+}
+
+document.querySelector('.b-20').onclick = t20;
